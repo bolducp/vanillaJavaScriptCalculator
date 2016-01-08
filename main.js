30b5ea7954b6561ed9f8661b2705eeae6d99908a
Paige Bolduc <paigebolduc@gmail.com>
2016-01-08 00:35:41 -0800

    add additional functionality and slightly improved styling

--- main.js
+++ main.js
@@ -2,103 +2,159 @@
 
 document.addEventListener('DOMContentLoaded', function(){
 
-    var num1 = "", num2 = "", operator = "", total = 0, operating = false, calculated = false;
+    var num1 = 0, num2 = 0, operator = "", total = 0, operating = false, calculated = false, adjuster = "";
     var outPut = document.getElementById("outPutDisplay");
 
+//Event Listeners
     var numbers = document.getElementsByClassName("num");
     for(var i = 0 ; i < numbers.length ; i++){
-        numbers[i].addEventListener("click", function(event){
-            var clickedNum = event.target.innerHTML;
-            console.log("num clicked", clickedNum);
-
-            if (operating === false ){
-                console.log("num1", num1);
-                num1 += clickedNum;
-                outPut.innerHTML = num1;
-              }
-
-            else if (operating === true){
-                console.log("num2", num2);
-                num2 += clickedNum;
-                outPut.innerHTML = num2;
-            } else {
-                outPut.innerHTML = "Error."
-            }
-          })
+        numbers[i].addEventListener("click", handleNumbers)
         }
 
-
     var operators = document.getElementsByClassName("operator");
-
     for (var i = 0; i < operators.length; i++){
-        operators[i].addEventListener("click", function(event) {
+        operators[i].addEventListener("click", handleOperators);
+    }
 
-          if (calculated === true) {
-              console.log("second calculation")
-              num1 = total;
-              num2 = "";
-              operator = event.target.innerHTML;
-              outPut.innerHTML = operator;
-              operating = true;
-              calculated = false;
-              console.log("num1: ", num1,"num2: ", num2, "operator: ", operator, "total: ", total, "operating", operating)
-
-          }
-            else if (operating === true) {
-              outPut.innerHTML = "invalid value";
-            } else {
-            operator = event.target.innerHTML;
-            operating = true;
-            console.log("operator clicked", operator);
-            outPut.innerHTML = operator;
-          }
-        });
-      }
+    var numAdjusters = document.getElementsByClassName("numAdjuster");
+    for (var i = 0; i < numAdjusters.length; i++){
+        numAdjusters[i].addEventListener("click", handleAdjusters);
+    }
 
     document.getElementById("equals").addEventListener("click", calculateTotal);
     document.getElementById("reset").addEventListener("click", reset);
 
 
+//other functions
+
+    function handleNumbers(event){
+       var clickedNum = event.target.innerHTML;
+       if (operating === false ){
+           num1 += clickedNum;
+           outPut.innerHTML = num1;
+         }
+       else if (operating === true){
+           num2 += clickedNum;
+           outPut.innerHTML = num2;
+       } else {
+           outPut.innerHTML = "Error."
+       }
+     }
 
 
-
-//event listeners here
-
-function calculateTotal(){
-    num1 = parseFloat(num1), num2 = parseFloat(num2);
-    if (num1 === NaN || num2 === NaN || operator === "") {
-        outPut.innerHTML = "Please enter a legit operation";
-        resetValues();
-        console.log("num1: ", num1,"num2: ", num2, "operator: ", operator, "total: ", total)
-      } else {
-          switch(operator){
-              case "/":
-                  total = num1 / num2;
-                  console.log("total", total)
-                  break;
-              case "x":
-                  total = num1 * num2;
-                  break;
-              case "-":
-                  total = num1 - num2;
-                  break;
-              case "+":
-                  total = num1 + num2;
-                  break;
-              default:
-                  total = "Please enter a legit operation";
-                  reset();
-                  console.log("num1: ", num1,"num2: ", num2, "operator: ", operator, "total: ", total)
-
-          }
-          outPut.innerHTML = total;
-          calculated = true;
+    function handleOperators(event) {
+      if (calculated === true) {
+          num1 = total;
+          num2 = "";
+          operator = event.target.innerHTML;
+          outPut.innerHTML = operator;
+          operating = true, calculated = false;
         }
+        else if (operating === true) {
+          outPut.innerHTML = "invalid value";
+        } else {
+        operator = event.target.innerHTML;
+        operating = true;
+        outPut.innerHTML = operator;
       }
+    }
 
-function reset(){
-  num1 = "", num2 = "", operator = "", total = 0, operating = false, calculated = false;
-  outPut.innerHTML = "0";
-}
+    //
+    //
+    // function handleOperators(event) {
+    //     if (operating === true) {
+    //       outPut.innerHTML = "invalid value";
+    //     }
+    //     else {
+    //         if (calculated === true) {
+    //           num1 = total;
+    //           num2 = "";
+    //           calculated = false;
+    //         }
+    //         operator = event.target.innerHTML;
+    //         operating = true;
+    //         outPut.innerHTML = operator;
+    //       }
+    //     }
 
-})
+
+    function calculateTotal(){
+        num1 = parseFloat(num1), num2 = parseFloat(num2);
+        if (num1 === NaN || num2 === NaN || operator === "") {
+            outPut.innerHTML = "Please enter a valid operation";
+            reset();
+        } else {
+            switch(operator){
+                case "/":
+                    total = num1 / num2;
+                    break;
+                case "x":
+                    total = num1 * num2;
+                    break;
+                case "-":
+                    total = num1 - num2;
+                    break;
+                case "+":
+                    total = num1 + num2;
+                    break;
+                default:
+                    total = "Please enter a valid operation";
+                    reset();
+              }
+              outPut.innerHTML = total;
+              calculated = true;
+            }
+          }
+
+
+    function handleAdjusters(event){
+        adjuster = event.target.innerHTML, num1 = parseFloat(num1), num2 = parseFloat(num2);
+        var currentVal = outPut.innerHTML;
+
+        if (parseFloat(currentVal) === NaN){
+            outPut.innerHTML = "Not a valid entry";
+            reset();
+            return;
+        }
+        if (operating === false){
+            switch(adjuster){
+                case "+/-":
+                num1 = String(num1 * -1);
+                break;
+                case "%":
+                num1 = String(num1 / 100);
+                break;
+                case ".":
+                num1 += ".";
+                break;
+                default:
+                total = "Please enter a valid operation";
+                reset();
+              }
+                outPut.innerHTML = num1;
+        } else {
+            switch(adjuster){
+                case "+/-":
+                num2 = num2 * -1;
+                break;
+                case "%":
+                num2 = num2 / 100;
+                break;
+                case ".":
+                num2 += ".";
+                break;
+                default:
+                total = "Please enter a valid operation";
+                reset();
+              }
+                outPut.innerHTML = num2;
+              }
+            }
+
+
+    function reset(){
+      num1 = 0, num2 = 0, operator = "", total = 0, operating = false, calculated = false;
+      outPut.innerHTML = "0";
+    }
+
+  })
